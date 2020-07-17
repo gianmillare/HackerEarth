fc4d7f00570828c35d82042708fcd0a3b731b829
gianmillare <gianmillaregsm@gmail.com>
2020-07-16 21:45:12 -0700

    completed tutorial challenge and added gitignore

---
+++ python/basics_of_input_output.py
@@ -0,0 +1,11 @@
+# Basics of Input/Output
+# https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/tutorial/
+
+def main(num, s):
+    print(num * 2)
+    print(s)
+
+num = int(input())
+s = input()
+
+main(num, s)
